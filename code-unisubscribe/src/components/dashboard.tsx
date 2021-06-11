154069c439a4f10a16c2509d1d0638b240834aa8
NurmahammadCode <nurmahammaddn@code.edu.az>
2021-06-11 17:33:12 +0400

    conflicts were solved

--- code-unisubscribe/src/components/dashboard.tsx
+++ code-unisubscribe/src/components/dashboard.tsx
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import clsx from "clsx";
-import {
-  makeStyles,
-  useTheme,
-  Theme,
-  createStyles,
-} from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import ChevronRightIcon from "@material-ui/icons/ChevronRight";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import InboxIcon from "@material-ui/icons/MoveToInbox";
-import MailIcon from "@material-ui/icons/Mail";
-import DeleteIcon from "@material-ui/icons/Delete";
-import ListIcon from "@material-ui/icons/List";
-
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Paper from "@material-ui/core/Paper";
-import Button from "@material-ui/core/Button";
-
-import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-=======
 import React from 'react';
 import clsx from 'clsx';
 import { withStyles, makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
@@ -79,7 +35,6 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
->>>>>>> b25aba373c9ad072a8c37a02c0a97320c9d45be4
 
 import "./style.scss"
 
@@ -149,30 +104,10 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-<<<<<<< HEAD
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number
-) {
-  return { name, calories, fat, carbs, protein };
-=======
 function createData(companyname: string, price: number, edate: string) {
   return { companyname, price, edate };
->>>>>>> b25aba373c9ad072a8c37a02c0a97320c9d45be4
 }
 const rows = [
-<<<<<<< HEAD
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-];
-
-=======
   createData('Frozen yoghurt', 159, "15.05.2021"),
   createData('Ice cream sandwich', 237, "10.04.2021"),
   createData('Eclair', 262, "28.03.2021"),
@@ -208,7 +143,6 @@ const StyledTableRow = withStyles((theme: Theme) =>
 )(TableRow);
 
 
->>>>>>> b25aba373c9ad072a8c37a02c0a97320c9d45be4
 export default function PersistentDrawerLeft() {
   const classes = useStyles();
   const theme = useTheme();
@@ -244,53 +178,6 @@ export default function PersistentDrawerLeft() {
             className={clsx(classes.menuButton, open && classes.hide)}>
           <MenuIcon />
           </IconButton>
-<<<<<<< HEAD
-          <Typography variant="h6">News</Typography>
-          <Button
-            variant="contained"
-            color="secondary"
-            style={{ marginLeft: "auto" }}
-            onClick={handleClickOpen}
-          >
-            Add New Subscription
-          </Button>
-          <Dialog
-            open={show}
-            onClose={handleClose}
-            aria-labelledby="form-dialog-title"
-          >
-            <DialogTitle id="form-dialog-title">Add Subscription</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                To subscribe, please enter information correctly
-              </DialogContentText>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="companyname"
-                label="Company Name"
-                type="text"
-                fullWidth
-              />
-              <TextField
-                margin="dense"
-                id="price"
-                label="Subscription Price"
-                type="text"
-                fullWidth
-              />
-              <TextField
-                id="link"
-                label="Link"
-                type="text"
-                fullWidth
-                style={{ marginBottom: "1rem" }}
-              />
-              <label htmlFor="expirationDate" style={{ fontSize: "1rem" }}>
-                Expirition Date
-              </label>
-              <TextField margin="dense" id="date" type="date" fullWidth />
-=======
           <Typography variant="h6" >
              SUBSCRIPTION LIST
           </Typography>
@@ -339,7 +226,6 @@ export default function PersistentDrawerLeft() {
                   </Form>
                 )}
               </Formik>
->>>>>>> b25aba373c9ad072a8c37a02c0a97320c9d45be4
             </DialogContent>
             <DialogActions>
               <Button onClick={handleClose} style={{ fontWeight: "bold" }} color="primary">
@@ -373,15 +259,8 @@ export default function PersistentDrawerLeft() {
         <List>
           {["Category"].map((text, index) => (
             <>
-<<<<<<< HEAD
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 !== 0 ? <DeleteIcon /> : <ListIcon />}
-                </ListItemIcon>
-=======
               <ListItem button >
                 <ListItemIcon>{index % 2 !== 0 ? <DeleteIcon /> : <ListIcon />}</ListItemIcon>
->>>>>>> b25aba373c9ad072a8c37a02c0a97320c9d45be4
                 <ListItemText primary={text} />
               </ListItem>
               <ListItem button>
@@ -397,15 +276,8 @@ export default function PersistentDrawerLeft() {
           ))}
           {["Trash"].map((text, index) => (
             <>
-<<<<<<< HEAD
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 !== 0 ? <ListIcon /> : <DeleteIcon />}
-                </ListItemIcon>
-=======
               <ListItem button >
                 <ListItemIcon>{index % 2 !== 0 ? <ListIcon /> : <DeleteIcon />}</ListItemIcon>
->>>>>>> b25aba373c9ad072a8c37a02c0a97320c9d45be4
                 <ListItemText primary={text} />
               </ListItem>
             </>
@@ -417,25 +289,6 @@ export default function PersistentDrawerLeft() {
           [classes.contentShift]: open,
         })}>
         <div className={classes.drawerHeader} />
-<<<<<<< HEAD
-        <TableContainer component={Paper} style={{ boxShadow: "1px" }}>
-          <Table
-            className={classes.table}
-            aria-label="simple table"
-            style={{ margin: "0 auto" }}
-          >
-            <TableHead>
-              <TableRow>
-                <TableCell style={{ fontWeight: "bold" }}>
-                  Company Name
-                </TableCell>
-                <TableCell style={{ fontWeight: "bold" }} align="right">
-                  Price
-                </TableCell>
-                <TableCell style={{ fontWeight: "bold" }} align="right">
-                  Expiration Date
-                </TableCell>
-=======
         <TableContainer component={Paper} style={{ boxShadow: "none" }}>
           <Table className={classes.table} aria-label="simple table" style={{ margin: "0 auto" }}>
             <TableHead>
@@ -443,7 +296,6 @@ export default function PersistentDrawerLeft() {
                 <TableCell style={{ fontWeight: "bold" }}>Company Name</TableCell>
                 <TableCell style={{ fontWeight: "bold" }} align="right">Price</TableCell>
                 <TableCell style={{ fontWeight: "bold" }} align="right">Expiration Date</TableCell>
->>>>>>> b25aba373c9ad072a8c37a02c0a97320c9d45be4
                 <TableCell align="right"></TableCell>
                 <TableCell align="right"></TableCell>
               </TableRow>
@@ -454,19 +306,6 @@ export default function PersistentDrawerLeft() {
                   <TableCell component="th" scope="row">
                     {row.companyname}
                   </TableCell>
-<<<<<<< HEAD
-                  <TableCell align="right">{row.calories}</TableCell>
-                  <TableCell align="right">{row.fat}</TableCell>
-                  <TableCell align="right" style={{ padding: "1rem 0" }}>
-                    <Button variant="contained" color="secondary">
-                      Edit
-                    </Button>
-                  </TableCell>
-                  <TableCell align="right" style={{ padding: "1rem 0 " }}>
-                    <Button variant="contained" color="primary">
-                      Delete
-                    </Button>
-=======
                   <TableCell align="right" >{row.price} $</TableCell>
                   <TableCell align="right" >{row.edate}</TableCell>
                   <TableCell align="right" style={{ padding: "0.5rem", width: 60 }}><Button variant="contained" onClick={handleClose} style={{ fontWeight: "bold" }} color="primary">
@@ -476,7 +315,6 @@ export default function PersistentDrawerLeft() {
                   <TableCell align="right" style={{ padding: "0.5rem", width: 150 }}><Button variant="contained" onClick={handleClose} style={{ fontWeight: "bold" }} color="secondary">
                     Unsubscribe
                   </Button>
->>>>>>> b25aba373c9ad072a8c37a02c0a97320c9d45be4
                   </TableCell>
                 </TableRow>
               ))}
