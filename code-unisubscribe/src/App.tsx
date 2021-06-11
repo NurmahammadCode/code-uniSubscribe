b27acad185e15551f862eda031a575e19bdbaf60
NurmahammadCode <nurmahammaddn@code.edu.az>
2021-06-11 14:28:43 +0400

    some changes

--- code-unisubscribe/src/App.tsx
+++ code-unisubscribe/src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
+import { HttpClient } from "./services/httpRequest";
 
 function App() {
-  return <div className="App"></div>;
+  const request = new HttpClient("http://172.28.0.48:8080/api/");
+  const handleTest = () => {
+    request.get("clients/1").then((response) => console.log(response));
+  };
+  return (
+    <div className="App">
+      <button onClick={handleTest}>Test</button>
+    </div>
+  );
 }
 
 export default App;
